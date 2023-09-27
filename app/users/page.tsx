e52d124067e850385730a67db7c9ce1336a06723
TomChaseParis <chasethomasparis@gmail.com>
2023-09-27 11:51:22 +0200

    fix(UsersPage): added a cache for communication with server

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,11 +6,14 @@ interface User {
 }
 
 const UsersPage = async () => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch('https://jsonplaceholder.typicode.com/users', { cache: 'no-store'});
+  // le cache permet d'afficher de communiquer avec le serveur et de rendre la page dynamique
   const users: User[] = await res.json();
+
   return (
     <>
       <h1>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
       <ul>
         {users.map(user => (
           <li key={user.id.toString()}>{user.name}</li>
