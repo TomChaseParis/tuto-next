4042b00282cfc9b3be0fd4ae97cd7f0342fc4718
TomChaseParis <chasethomasparis@gmail.com>
2023-09-27 11:17:04 +0200

    feat: Created first components of application

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,13 @@
+'use client' // add this line for button to work
+import React from 'react'
+import AddToCart from './AddToCart'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import Image from 'next/image'
+import Link from 'next/link'
+import ProductCard from './components/ProductCard'
 
 export default function Home() {
   return (
     <main>
       <h1>Hello</h1>
+      <Link href="/users/new">Users</Link>
+      <ProductCard />
     </main>
   )
 }
